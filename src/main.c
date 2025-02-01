6bc771fd3c7678988688da0225e3f0dfdaa486f4
E233-3639 <ryoogisoe233@gmail.com>
2025-02-01 17:29:18 +0900

    change: GitHub Actions の動作と main.c の実装を変更

--- src/main.c
+++ src/main.c
@@ -1,4 +1,13 @@
+#include <stdio.h>
+
 int main(void) {
     int i;
+    printf("start\n", i);
+    while (i < 10)
+    {
+        printf("number: %d\n", i);
+        i++;
+    }
+    printf("end\n", i);
     return 0;
 }
